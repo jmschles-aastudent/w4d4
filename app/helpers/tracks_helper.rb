ea26501c7069eea8a3a6aa2a4ddb2446ff39bd61
Jamie Schlessinger <james.schlessinger@gmail.com>
2013-06-06 22:21:27 -0700

    Add ugly lyrics

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,2 +1,13 @@
 module TracksHelper
+
+	def ugly_lyrics(lyrics)
+		music = CGI.unescapeHTML('&#9835;')
+		lines = lyrics.split("\r\n")
+		new_lines = lines.map do |line|
+			"#{music} #{h(line)}"
+		end
+		lyrs = new_lines.join('<br />')
+		lyrs.html_safe
+	end
+
 end
